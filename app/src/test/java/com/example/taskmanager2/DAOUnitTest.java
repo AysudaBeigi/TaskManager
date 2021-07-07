17adbfc9961e1f74a510cbf0fc72bb43c313834a
AysudaBeigi <AysudaBeigi@gmail.com>
2021-07-07 06:21:58 +0430

    Complete DAOUnitTest .

--- app/src/test/java/com/example/taskmanager2/DAOUnitTest.java
+++ app/src/test/java/com/example/taskmanager2/DAOUnitTest.java
@@ -3,9 +3,14 @@ package com.example.taskmanager2;
 import android.content.Context;
 
 import androidx.room.Room;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 import androidx.test.core.app.ApplicationProvider;
 
+import com.example.taskmanager2.database.TaskDBDAO;
+import com.example.taskmanager2.database.TaskManagerDatabase;
 import com.example.taskmanager2.database.UserDBDAO;
+import com.example.taskmanager2.model.Task;
+import com.example.taskmanager2.model.TaskState;
 import com.example.taskmanager2.model.User;
 
 import org.junit.After;
@@ -15,18 +20,24 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 @RunWith(JUnit4.class)
 public class DAOUnitTest {
 
     private UserDBDAO userDao;
-    private TestDatabase db;
+    private TaskDBDAO taskDao;
+    private TaskManagerDatabase db;
 
     @Before
     public void createDb() {
         Context context = ApplicationProvider.getApplicationContext();
-        db = Room.inMemoryDatabaseBuilder(context, TestDatabase.class).build();
-        userDao = db.getUserDao();
+        db = Room.inMemoryDatabaseBuilder(context, TaskManagerDatabase.class).build();
+        userDao = db.getUserDBDAO();
+        taskDao = db.getTaskDBDAO();
     }
 
     @After
@@ -36,19 +47,30 @@ public class DAOUnitTest {
 
     @Test
     public void writeUserAndReadInList() throws Exception {
-        User user = TestUtil.createUser(3);
-        user.setName("george");
-        userDao.insert(user);
-        List<User> byName = userDao.findUsersByName("george");
-        assertThat(byName.get(0), equalTo(user));
+        User user = new User("username", "password");
+        user.setUsername("george");
+        userDao.insertUser(user);
+        List<User> users = userDao.getUsers();
+        assertThat(users.get(0), equalTo(user));
+
+        user.setUsername("ali");
+        userDao.updateUser(user);
+        assertThat(users.get(0), equalTo(user));
+
+        User specialUser = userDao.getUser("username");
+        List<Task> userTasks=userDao.getTasks("username", TaskState.DOING);
+
+        Task task=new Task("username",TaskState.DOING);
+        taskDao.insertTask(task);
+        List<Task> tasks=taskDao.getTasks();
+        Task specialTask=taskDao.getTask("title");
+        assertThat(tasks.get(0),equalTo(task));
+
+        task.setTitle("new title");
+        taskDao.updateTask(task);
+        assertThat(tasks.get(0),equalTo(task));
+
+
     }
 }
 
-
-
-
-
-
-
-
-}
